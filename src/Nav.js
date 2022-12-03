0775ecd2f738ec4eedc1477a270176928bbca008
arshad2001 <62998839+arshad2001@users.noreply.github.com>
2022-12-03 15:01:30 +0700

    favicon

--- src/Nav.js
+++ src/Nav.js
@@ -5,7 +5,7 @@ import shadocodeLogo from "./assets/shadocode-logo.png";
 
 function Nav() {
   return (
-    <div className=" md:w-screen w-min-[400px] md:text-gray-400 h-fit md:px-12 px-6 md:py-8 py-4 items-center justify-items-cente grid grid-cols-2 md:text-[16px] text-[10px] text-blue-400 fixed z-50">
+    <div className=" w-screen  md:text-gray-400 h-fit md:px-12 px-6 md:py-8 py-4 items-center justify-items-cente grid grid-cols-2 md:text-[16px] text-[10px] text-blue-400 fixed z-50">
       <div className="items-center justify-self-start flex cursor-pointer text-black ">
         <img
           src={shadocodeLogo}
@@ -16,7 +16,7 @@ function Nav() {
           shadocode
         </a>
       </div>
-      <div className="justify-self-end ">
+      <div className="justify-self-end hidden md:block">
         <ul className="h-full flex space-x-4 font-medium ">
           <li className="cursor-pointer hover:text-black" src="./Work.js">
             home
@@ -25,6 +25,12 @@ function Nav() {
           <li className="cursor-pointer hover:text-black">work</li>
           <li className="cursor-pointer hover:text-teal-400">hobby</li>
         </ul>
+      </div>
+      <div className="justify-self-end block md:hidden">
+        <img
+          src={shadocodeLogo}
+          className="md:max-w-[28px] mr-2 max-w-[20px]"
+        ></img>
       </div>
     </div>
   );
