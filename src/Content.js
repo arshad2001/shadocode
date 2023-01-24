a7df1d7a52b7d897fd062dcf326413fb339cea1c
arshad2001 <62998839+arshad2001@users.noreply.github.com>
2023-01-24 12:17:35 +0700

    new stuff today SURUIURBOI:Eb

--- src/Content.js
+++ src/Content.js
@@ -2,6 +2,9 @@ import logo from "./logo.svg";
 import "./App.css";
 import auroraPic from "./assets/aurora.jpg";
 import ContentBlocks from "./ContentBlocks";
+import riperAll from "./assets/riperAll_porto.png";
+
+const ballin = [{ niggah: "riperAll" }];
 
 function Content(props) {
   return (
@@ -40,11 +43,33 @@ function Content(props) {
       </div>
 
       <div className="grid md:grid-cols-3 grid-cols-1 grid-flow-row gap-[1px] md:px-12 px-6 h-fit py-16">
-        <ContentBlocks />
-        <ContentBlocks />
-        <ContentBlocks />
-        <ContentBlocks />
-        <ContentBlocks />
+        <a
+          id="riperAll"
+          href="https://www.figma.com/file/52oBGEDAnr3j8hE49ABkw6/RiperAll---User-Testing?t=u3X7YQ3dnzVP0E0v-6"
+        >
+          {ballin.map(({ nuts }) => (
+            <ContentBlocks niggah={nuts} />
+          ))}
+          <ContentBlocks />
+        </a>
+        <a
+          id="websiteHimatekkom"
+          href="https://www.figma.com/file/rnaguP8MjMPi9KsEQMNb7Y/Website-Himatekkom---Convergence-team-library?t=u3X7YQ3dnzVP0E0v-6"
+        >
+          <ContentBlocks />
+        </a>
+        <a href="https://www.instagram.com/direct/t/340282366841710300949128218359528260178">
+          <ContentBlocks />
+        </a>
+        <a href="https://www.instagram.com/direct/t/340282366841710300949128218359528260178">
+          <ContentBlocks />
+        </a>
+        <a href="https://www.instagram.com/direct/t/340282366841710300949128218359528260178">
+          <ContentBlocks />
+        </a>
+        <a href="https://www.instagram.com/direct/t/340282366841710300949128218359528260178">
+          <ContentBlocks />
+        </a>
       </div>
 
       <div
--- src/ContentBlocks.js
+++ src/ContentBlocks.js
@@ -1,12 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import riperAll from "./assets/riperAll_porto.png";
 
-function ContentBlocks() {
+function ContentBlocks({nuts}) {
   return (
     <div class="h-fit overflow-hidden hover:rotate-2 hover:-translate-y-2 hover:shadow-2xl hover:shadow-teal-200/80 hover:border-teal-200 hover:border-2 transition duration-200 ease-in-out dark:bg-gray-900 bg-gray-300">
-      <div
+      <img
         class="h-44 md:h-56 w-full object-cover object-center"
-        src="https://dummyimage.com/720x400"
+        src={"nuts"}
         alt="blog"
       />
     </div>
